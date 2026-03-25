543faab86dc6d18e56bfa06d0e3655ad188bc4d9
Epotat <95963230+Epotat@users.noreply.github.com>
2026-03-25 13:53:07 +0100

    removed the js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const container = document.querySelector('.navbar');
-const text = document.getElementById('hometext');
 
-function resizeText() {
-  const containerHeight = container.clientHeight;
-
-  // Set font-size as a percentage of navbar height
-  const fontSize = containerHeight * 0.9; // 70% of navbar height
-  text.style.fontSize = fontSize + 'px';
-}
-
-// Run on load and on window resize
-window.addEventListener('load', resizeText);
-window.addEventListener('resize', resizeText);
-resizeText()
